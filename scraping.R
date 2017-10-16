89b16bd8f722049e23c90391f76c9af179a623ed
Lapparov Valentin <vlapparov@mail.ru>
2017-10-16 10:29:53 +0200

    I read a table from Wiki

--- scraping.R
+++ scraping.R
@@ -1,7 +1,10 @@
 library("rvest")
 library("xml2")
 
-my_page <- read_html("https://en.wikipedia.org/wiki/President_of_Russia")
-html_nodes(x = my_page, css = ".dt-blog-shortcode > .wf-cell")
+my_page <- read_html('https://en.wikipedia.org/wiki/President_of_Russia')
+table <- html_table(my_page,fill = TRUE)
+data <- table[[3]]
+html_nodes(my_page)
+# html_nodes(x = my_page, css = ".dt-blog-shortcode > .wf-cell")
 articles <- html_nodes(x = my_page, css = "[data-name]")
 # my_page <- read_html("http://thinkr.fr")
