8393f0894cf5819b081638601bbd3acf7f4d64f7
Lapparov Valentin <vlapparov@mail.ru>
2017-10-16 14:31:25 +0200

    Starting project with Data from Kaggle

--- scraping.R
+++ scraping.R
@@ -1,21 +1,19 @@
 library("rvest")
 library("xml2")
-library(stringr)
+library("stringr")
 library("dplyr")
 
-my_page <- read_html('https://en.wikipedia.org/wiki/President_of_Russia')
-table <- html_table(my_page,fill = TRUE)
-data <- table[[3]]
-html_nodes(my_page)
-cheese_nodes <- html_nodes(x = my_page, css = ".summary-points")
+# my_page <- read_html('https://en.wikipedia.org/wiki/President_of_Russia')
+# table <- html_table(my_page,fill = TRUE)
+# data <- table[[3]]
+# html_nodes(my_page)
+my_page <- read.csv("https://www.kaggle.com/uciml/student-alcohol-consumption/downloads/student-mat.csv")
+cheese_nodes <- html_nodes(x = my_page, css = ".data-view__button-download")
 text_from_page <- html_text(cheese_nodes, trim = TRUE)
 clean <- str_replace_all(text_from_page, "([\t])", "")
 clean
-dupl <- gsub(x = clean, pattern = "\n", replacement = "~")
-dupl <- gsub(x = dupl, pattern = "~~", replacement = "~")
-dupl
+# dupl <- gsub(x = clean, pattern = "\n", replacement = "~")
+# dupl
 list <- strsplit(x = clean, split = "\n")
-len <- list[[1]] %>% match("")
-# t <- as.data.frame(text_from_page)
-articles <- html_nodes(x = my_page, css = "[data-name]")
-my_page <- read_html("https://www.cheese.com/feta/")
+t <- as.data.frame(list)
+
