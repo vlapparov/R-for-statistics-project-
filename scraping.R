ea3de1b1ed226411bf44e55c485ecd0378d7484c
Lapparov Valentin <vlapparov@mail.ru>
2017-10-16 10:22:10 +0200

    Trying to read the web-page

---
+++ scraping.R
@@ -0,0 +1,7 @@
+library("rvest")
+library("xml2")
+
+my_page <- read_html("https://en.wikipedia.org/wiki/President_of_Russia")
+html_nodes(x = my_page, css = ".dt-blog-shortcode > .wf-cell")
+articles <- html_nodes(x = my_page, css = "[data-name]")
+# my_page <- read_html("http://thinkr.fr")
